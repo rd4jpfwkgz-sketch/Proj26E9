6358e81cbff09f821dd5087f68520f5c0d124549
Fernando Gomes <Fernando Gomes@LAPTOP-D77LTEB4.home>
2026-05-19 23:49:42 +0100

    Adicionei os gets e sets na ocorrencia, para fazer o metodo da edicao

--- Proj26E9/src/Gerir_ocorrencia.java
+++ Proj26E9/src/Gerir_ocorrencia.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Gerir_ocorrencia {
 	private ArrayList<Ocorrencia> lista_correncias;
@@ -47,5 +48,56 @@ public class Gerir_ocorrencia {
 	public void registar_ocorrencia(Ocorrencia o) {
 		lista_correncias.add(o);
 	}
+	public void editar_ocorrencias() {
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Insira o titulo da ocorrência que pretende editar");
+		String titulo=sc.nextLine();
+		for (Ocorrencia o:lista_correncias) {
+			if(o.getTitulo().equalsIgnoreCase(titulo)) {
+				int opcao;
+				do {
+					System.out.println("Selecione o que pretende editar:");
+					System.out.println("1-editar titulo");
+					System.out.println("2-editar descrição");
+					System.out.println("3-editar Estado");
+					System.out.println("4-editar localização");
+					System.out.println("5-editar nivel de urgência");
+					System.out.println("0- sair");
+					opcao=sc.nextInt();
+					sc.nextLine();
+					switch(opcao) {
+					case 1:
+						System.out.println("Novo Titulo:");
+						o.setTitulo(sc.nextLine());
+						break;
+					case 2:
+						System.out.println("Nova Descrição:");
+						o.setDescricao(sc.nextLine());
+						break;
+					case 3:
+						System.out.println("Novo Estado");
+						o.setEstado(sc.nextLine());
+						break;
+					case 4:
+						System.out.println("Nova Localização:");
+						o.setLocalizacao(sc.nextLine());
+						break;
+					case 5:
+						System.out.println("Novo nível de urgência:");
+						o.setNivel_urgencia(sc.nextInt());
+						break;
+					case 0:
+						System.out.println("Edição terminada!");
+						break;
+					
+					}	
+				}while (opcao !=0);
+				return;
+				
+			}
+		}
+		System.out.println("Esta ocorrência não existe");
+	}
+	
 	
 }
